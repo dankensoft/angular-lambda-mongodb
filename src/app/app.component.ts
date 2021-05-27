370155062a69de43285734f823d4766af680a782
Danniels Castillo <danniels.magana@gmail.com>
2021-05-26 18:09:14 -0600

    GET Data AWS Lambda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todoapp';
+
+  items = [];
+  constructor(private _http: HttpClient){
+
+  }
+
+  ngOnInit(){
+    var url = "https://srrps9t3c8.execute-api.ca-central-1.amazonaws.com/prod/todoitems";
+    var me = this;
+    this._http.get(url).toPromise().then(
+      function(result: any) {
+        // console.log(JSON.parse(result.body).items);
+        me.items = JSON.parse(result.body).items;
+      },
+      function(error) {
+        console.error(error);
+      }
+    )
+  }
 }
